8e127649f66f618b65610611cd7401460f6fcf1d
John Erik Ekberg <JOEK@adm.ku.dk>
2022-11-17 15:43:20 +0100

    #19: convert simple repeats to bar view model

--- src/logical-view/view-model/convert-repeat.test.ts
+++ src/logical-view/view-model/convert-repeat.test.ts
@@ -1,19 +1,60 @@
-import { Meter, MeterFactory } from './../../model/states/meter';
-import { Key } from './../../model/states/key';
-import { Clef } from './../../model/states/clef';
+import { BarType } from './score-view-model';
+import { Time } from './../../model/rationals/time';
+import { RepeatDef } from './../../model/score/repeats';
 import { expect } from 'chai';
-import { keyToView } from './convert-key';
-import { meterToView } from './convert-meter';
+import { repeatsToView } from './convert-repeat';
 
 describe('View model: Repeats', () => {
 
+    it('should convert a repeat (from start time) to view model', () => {
+        const repeats = [{ from: Time.StartTime, to: Time.newAbsolute(5, 1) } as RepeatDef];
 
-    beforeEach(() => { 
-        //
+        const answer = repeatsToView(repeats);
+
+        expect(answer).to.have.length(1);
+        expect(answer[0].repeatEnd).to.be.true;
+        expect(answer[0]).to.deep.eq({ barType: BarType.None, repeatEnd: true, time: Time.newAbsolute(5, 1) });
     });
 
-    it('should convert a repeat to view model', () => {
-        //
+
+    it('should convert a repeat (from later than start time) to view model', () => {
+        const repeats = [{ from: Time.newAbsolute(2, 1), to: Time.newAbsolute(6, 1) } as RepeatDef];
+
+        const answer = repeatsToView(repeats);
+
+        expect(answer).to.have.length(2);
+        expect(answer[0]).to.deep.eq({ barType: BarType.None, repeatStart: true, time: Time.newAbsolute(2, 1) });
+        expect(answer[1]).to.deep.eq({ barType: BarType.None, repeatEnd: true, time: Time.newAbsolute(6, 1) });
+    });
+
+    
+    it('should convert two repeats (not consecutive) to view model', () => {
+        const repeats = [
+            { from: Time.StartTime, to: Time.newAbsolute(2, 1) } as RepeatDef,
+            { from: Time.newAbsolute(5, 1), to: Time.newAbsolute(6, 1) } as RepeatDef
+        ];
+
+        const answer = repeatsToView(repeats);
+
+        expect(answer).to.have.length(3);
+        expect(answer[0]).to.deep.eq({ barType: BarType.None, repeatEnd: true, time: Time.newAbsolute(2, 1) });
+        expect(answer[1]).to.deep.eq({ barType: BarType.None, repeatStart: true, time: Time.newAbsolute(5, 1) });
+        expect(answer[2]).to.deep.eq({ barType: BarType.None, repeatEnd: true, time: Time.newAbsolute(6, 1) });
+    });
+
+
+    
+    it('should convert two repeats (consecutive) to view model', () => {
+        const repeats = [
+            { from: Time.StartTime, to: Time.newAbsolute(5, 1) } as RepeatDef,
+            { from: Time.newAbsolute(5, 1), to: Time.newAbsolute(6, 1) } as RepeatDef
+        ];
+
+        const answer = repeatsToView(repeats);
+
+        expect(answer).to.have.length(2);
+        expect(answer[0]).to.deep.eq({ barType: BarType.None, repeatStart: true, repeatEnd: true, time: Time.newAbsolute(5, 1) });
+        expect(answer[1]).to.deep.eq({ barType: BarType.None, repeatEnd: true, time: Time.newAbsolute(6, 1) });
     });
 
 
---
+++ src/logical-view/view-model/convert-repeat.ts
@@ -0,0 +1,31 @@
+import { IndexedMap } from './../../tools/time-map';
+import { AbsoluteTime, Time } from './../../model/rationals/time';
+import { BarType, BarViewModel } from './score-view-model';
+import { RepeatDef } from './../../model/score/repeats';
+
+export interface TimedBarViewModel extends BarViewModel {
+    time: AbsoluteTime;
+}
+
+export function repeatsToView(repeats: RepeatDef[]): TimedBarViewModel[] {
+    const map = new IndexedMap<BarViewModel, AbsoluteTime>(Time.sortComparison, () => ({ barType: BarType.None }));
+
+    repeats.forEach(repeat => {
+        if (repeat.from.numerator > 0) {
+            const item = map.get(repeat.from);
+            item.repeatStart = true;
+            //res.push({ barType: BarType.None, repeatStart: true, time: repeat.from });
+        }
+        //res.push({ barType: BarType.None, repeatEnd: true, time: repeat.to });
+        const item = map.get(repeat.to);
+        item.repeatEnd = true;
+    });
+
+    const res = [] as TimedBarViewModel[];
+    map.forEach((index, value) => {
+        const a = {... value, time: index};
+        res.push(a);
+    });
+
+    return res;
+}
