1cd442f2bf077b27c62feafbffdf57a33c116571
John Erik Ekberg <JOEK@adm.ku.dk>
2022-11-21 11:19:06 +0100

    #9: listing note expressions

---
+++ src/model/notes/note-expressions.ts
@@ -0,0 +1,241 @@
+export const noteExpressions = [
+    {
+        'name': 'fermata',
+        'upDown': true,
+        'lily': '\\fermata'      
+    },
+    {
+        'name': 'shortfermata',
+        'upDown': true,
+        'lily': '\\shortfermata'      
+    },
+    {
+        'name': 'longfermata',
+        'upDown': true,
+        'lily': '\\longfermata'      
+    },
+    {
+        'name': 'verylongfermata',
+        'upDown': true,
+        'lily': '\\verylongfermata'      
+    },
+    {
+        'name': 'veryshortfermata',
+        'upDown': true,
+        'lily': '\\veryshortfermata'      
+    },
+    {
+        'name': 'henzeshortfermata',
+        'upDown': true,
+        'lily': '\\henzeshortfermata'      
+    },
+    {
+        'name': 'henzelongfermata',
+        'upDown': true,
+        'lily': '\\henzelongfermata'      
+    },
+    {
+        'name': 'thumb',
+        'lily': '\\thumb'      
+    },
+    {
+        'name': 'sforzato',
+        'lily': '\\accent',
+        'shortLily': '->'
+    },
+    {
+        'name': 'espr',
+        'lily': '\\espressivo'      
+    },
+    {
+        'name': 'staccato',
+        'lily': '\\staccato',
+        'shortLily': '-.'
+    },
+    {
+        'name': 'staccatissimo',
+        'upDown': true,
+        'lily': '\\staccatissimo',
+        'shortLily': '-!'
+    },
+    {
+        'name': 'tenuto',
+        'lily': '\\tenuto',
+        'shortLily': '--'
+    },
+    {
+        'name': 'portato',
+        'upDown': true,
+        'lily': '\\portato',
+        'shortLily': '-_'
+    },
+    {
+        'name': 'marcato',
+        'upDown': true,
+        'lily': '\\marcato',
+        'shortLily': '-^'
+    },
+    {
+        'name': 'open',
+        'lily': '\\open'      
+    },
+    {
+        'name': 'halfopen',
+        'lily': '\\halfopen'      
+    },
+    {
+        'name': 'halfopenvertical'              
+    },
+    {
+        'name': 'stopped',
+        'lily': '\\stopped',
+        'shortLily': '-+'
+    },
+    {
+        'name': 'upbow',
+        'lily': '\\upbow'      
+    },
+    {
+        'name': 'downbow',
+        'lily': '\\downbow'      
+    },
+    {
+        'name': 'reverseturn',
+        'lily': '\\reverseturn'      
+    },
+    {
+        'name': 'turn',
+        'lily': '\\turn'      
+    },
+    {
+        'name': 'slashturn',
+        'lily': '\\slashturn'      
+    },
+    {
+        'name': 'haydnturn',
+        'lily': '\\haydnturn'      
+    },
+    {
+        'name': 'trill',
+        'lily': '\\trill'      
+    },
+    {
+        'name': 'upedalheel',
+        'lily': '\\lheel'      
+    },
+    {
+        'name': 'dpedalheel',
+        'lily': '\\rheel'      
+    },
+    {
+        'name': 'upedaltoe',
+        'lily': '\\ltoe'      
+    },
+    {
+        'name': 'dpedaltoe',
+        'lily': '\\rtoe'      
+    },
+    {
+        'name': 'flageolet',
+        'lily': '\\flageolet'      
+    },
+    {
+        'name': 'segno',
+        'lily': '\\segno'      
+    },
+    {
+        'name': 'varsegno'              
+    },
+    {
+        'name': 'coda',
+        'lily': '\\coda'      
+    },
+    {
+        'name': 'varcoda',
+        'lily': '\\varcoda'      
+    },
+    {
+        'name': 'rcomma'              
+    },
+    {
+        'name': 'lcomma'              
+    },
+    {
+        'name': 'rvarcomma'              
+    },
+    {
+        'name': 'lvarcomma'              
+    },
+    {
+        'name': 'arpeggio'              
+    },
+    {
+        'name': 'trill_element'              
+    },
+    {
+        'name': 'arpeggio.arrow.M1'              
+    },
+    {
+        'name': 'arpeggio.arrow.1'              
+    },
+    {
+        'name': 'trilelement'              
+    },
+    {
+        'name': 'prall',
+        'lily': '\\prall'      
+    },
+    {
+        'name': 'mordent',
+        'lily': '\\mordent'      
+    },
+    {
+        'name': 'prallprall',
+        'lily': '\\prallprall'      
+    },
+    {
+        'name': 'prallmordent',
+        'lily': '\\prallmordent'      
+    },
+    {
+        'name': 'upprall',
+        'lily': '\\upprall'      
+    },
+    {
+        'name': 'upmordent',
+        'lily': '\\upmordent'      
+    },
+    {
+        'name': 'pralldown',
+        'lily': '\\pralldown'      
+    },
+    {
+        'name': 'downprall',
+        'lily': '\\downprall'      
+    },
+    {
+        'name': 'downmordent',
+        'lily': '\\downmordent'      
+    },
+    {
+        'name': 'prallup',
+        'lily': '\\prallup'      
+    },
+    {
+        'name': 'lineprall',
+        'lily': '\\lineprall'      
+    },
+    {
+        'name': 'caesura.curved'              
+    },
+    {
+        'name': 'caesura.straight'              
+    },
+    {
+        'name': 'tickmark'              
+    },
+    {
+        'name': 'snappizzicato',
+        'lily': '\\snappizzicato'      
+    }
+];
