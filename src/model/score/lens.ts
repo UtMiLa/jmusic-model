1102f0b8e02e432b577cc58ab8242efba80bf51d
John Erik Ekberg <JOEK@adm.ku.dk>
2023-05-31 16:36:15 +0200

    thinking loud in a comment

--- src/model/score/lens.ts
+++ src/model/score/lens.ts
@@ -42,6 +42,28 @@ export function noteLens(time: AbsoluteTime): NoteLens {
         
     );
 }
+/*
+
+    wanted behaviour:
+        return a path to the indicated object
+            ['vars', 'myVar', 5]
+            ['score', 'staves', 1, 'voices', 1, 5]
+        also a (accumulated) function
+            compose(transpose('minor third up'), augment(2/1))
+        and - if applicable - inverse function
+            compose(augment(1/2), transpose('minor third down'))
+    then the lens is like:
+        R.lens(
+            R.pipe(R.path(thePath), theFunction),
+            R.pipe(theReverseFunction, R.assocPath(thePath))
+        )
+    future thoughts:
+        when writing to a variable or function, select
+            overwriting original variable
+            create a new function with single-modifications
+            remove link to function/variable and make the notes part of sequence
+
+*/
 
 export function view(lens: Lens, project: ProjectDef): StaffDef[] {
     return lens.getPaths().map(
